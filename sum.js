d09059312bc7927c55cd64940cd45ea769c73f21
ZongHong <make75682@gmail.com>
2022-07-31 17:04:57 +0800

    [fixed]debug sum.js

--- sum.js
+++ sum.js
@@ -6,10 +6,13 @@ function sum(nums) {
     return result;
 }
 
-Array.prototype.doFilte = function (str) {
+Array.prototype.doFilter = function (callback) {
+    if (typeof (callback) != "function") {
+        return "is not function";
+    }
     result = [];
     for (let item of this) {
-        if (item === str) {
+        if (callback(item)) {
             result.doPush(item);
             // console.log(result);
         }
@@ -54,28 +57,95 @@ Array.prototype.doReduce = function (callback, initVal = 0) {
     }
     let result = initVal;
     for (let item of this) {
-        result = callback(result, this[item], item, this);
+        if (result == 0) {
+            result = item;
+            continue;
+        }
+        result = callback(result, item, item, this);
         // console.log(result);
     }
-    // return result
+    return result;
 }
 
-Array.prototype.doConcat = function (arr2) {
+Array.prototype.doConcat = function (...items) {
     let result = this;
-    for (let item of arr2) {
-        result.doPush(item);
+
+    for (let item of items) {
+        if (typeof (item) != "object")
+            result.doPush(item);
+
+        for (let i of item) {
+            result.doPush(i);
+        }
     }
-    return result
+    return result;
 }
 
-let test = [1, 1, 2, 3];
-let test2 = [1, 1, 2, 3, 5, 6];
-let t = ["t", "d", "s"];
-let newt = test.doConcat(test2);
-// console.log(test2);
-// console.log(test);
-// console.log(test2);
-// console.log(test.doReduce((s,c) => s+c));
-// console.log(test.concat(e => e < 2));
 
+
+let arr = [
+    {
+        id: 1,
+        type: 'A',
+        price: 100,
+    },
+    {
+        id: 2,
+        type: 'B',
+        price: 200,
+    },
+    {
+        id: 5,
+        type: 'A',
+        price: 150,
+    },
+    {
+        id: 6,
+        type: 'D',
+        price: 250,
+    },
+];
+console.log('doFilter:');
+console.log(arr.doFilter(item => item.type == 'A'));
+console.log('filter:');
+console.log(arr.filter(item => item.type == 'A'));
+
+console.log();
+
+console.log('[3,5,6].opush(0)');
+console.log('answer:');
+let arr2 = [3, 5, 6];
+arr2.doPush(0);
+console.log(arr2);
+
+
+console.log();
+
+console.log('doFind:');
+console.log(arr.doFind(item => item.price > 180));
+console.log('find:');
+console.log(arr.find(item => item.price > 180));
+
+console.log();
+
+console.log('doMap:')
+console.log(arr.doMap(item => item.price * 10));
+console.log('map:')
+console.log(arr.map(item => item.price * 10));
+
+console.log();
+
+console.log('doReduce:');
+console.log(arr2.doReduce((start, next, index, a) => start + next));
+console.log('reduce:');
+console.log(arr2.reduce((start, next) => start + next));
+
+console.log();
+
+
+let test = [9, 10, 11, 12];
+console.log('doConcat:');
+console.log(arr2.doConcat(test));
+console.log('concate:');
+console.log(arr2.concat(test));
 
