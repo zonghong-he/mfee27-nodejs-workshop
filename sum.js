3199c934803c91671e2250e93965aed4b9ed2656
ZongHong <make75682@gmail.com>
2022-07-31 15:49:33 +0800

    [docs]arrange sum.js

--- sum.js
+++ sum.js
@@ -1,5 +1,3 @@
-const { type } = require("os");
-
 function sum(nums) {
     let result = 0;
     for (let i = 0; i <= nums; i++) {
@@ -13,7 +11,7 @@ Array.prototype.doFilte = function (str) {
     for (let item of this) {
         if (item === str) {
             result.doPush(item);
-            console.log(result)
+            // console.log(result);
         }
     }
     return result;
@@ -30,9 +28,9 @@ Array.prototype.doFind = function (callback) {
         return "is not function";
     }
     for (let item of this) {
-        // console.log(callback(item))
+        // console.log(callback(item));
         if (callback(item)) {
-            return item
+            return item;
         }
     }
 
@@ -44,36 +42,39 @@ Array.prototype.doMap = function (callback) {
     }
     let result = [];
     for (let item of this) {
-        // console.log(callback(item))
+        // console.log(callback(item));
         result.doPush(callback(item));
     }
     return result;
 }
 
 Array.prototype.doReduce = function (callback, initVal = 0) {
+    if (typeof (callback) != "function") {
+        return "is not function";
+    }
     let result = initVal;
     for (let item of this) {
         result = callback(result, this[item], item, this);
-        console.log(result)
+        // console.log(result);
     }
     // return result
 }
 
-Array.prototype.doConcat=function(arr2){
-    let result=this;
-    for(let item of arr2){
+Array.prototype.doConcat = function (arr2) {
+    let result = this;
+    for (let item of arr2) {
         result.doPush(item);
     }
     return result
 }
 
-let test = [1, 1, 2, 3]
-let test2 = [1, 1, 2, 3,5,6]
-let t = ["t", "d", "s"]
-let newt=test.doConcat(test2)
-console.log(test2)
-// console.log(test)
-// console.log(test2)
+let test = [1, 1, 2, 3];
+let test2 = [1, 1, 2, 3, 5, 6];
+let t = ["t", "d", "s"];
+let newt = test.doConcat(test2);
+// console.log(test2);
+// console.log(test);
+// console.log(test2);
 // console.log(test.doReduce((s,c) => s+c));
 // console.log(test.concat(e => e < 2));
 
