e097b50bccc3adaec223dadc3c78e996fe704019
ZongHong <make75682@gmail.com>
2022-08-14 11:51:01 +0800

    [feat]carwler4加入promise版本

--- crawler/crawler4.js
+++ crawler/crawler4.js
@@ -2,24 +2,24 @@
 // 2. 從檔案中讀取股票代碼
 const axios = require('axios');
 const moment = require('moment');
+const fs = require('fs');
 
 // 開始抓資料
 // 2330 台積電
 // 2603 長榮
-const fs = require('fs');
-let stockNo = fs.readFileSync('stockNo.txt', 'utf8', (err, data) => {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data);
-  return data;
-});
-console.log(stockNo);
 // TODO: 需要從 stock.txt 的檔案裡讀取股票代碼
 let queryDate = moment().format('YYYYMMDD'); //'20220814';
 // axios.get(url, 設定)
 (async () => {
-  try {    
+  try {
+    let stockNo = await fs.promises.readFile('stockNo.txt', 'utf8', (err, data) => {
+      if (err) {
+        return console.log(err);
+      }
+      // console.log(data);
+      return data;
+    });
+    console.log(stockNo);
     let response = await axios.get(`https://www.twse.com.tw/exchangeReport/STOCK_DAY`, {
       params: {
         response: 'json',
