b515607a8a5d9345fd2aaaa619acbb92357526eb
ZongHong <make75682@gmail.com>
2022-08-14 14:37:17 +0800

    [feat]search name

---
+++ crawler/crawler4.js
@@ -0,0 +1,37 @@
+// 1. 自動取得今日日期 （可能利用 cron 排程工具 系統每日自動執行）
+// 2. 從檔案中讀取股票代碼
+const axios = require('axios');
+const moment = require('moment');
+const fs = require('fs');
+
+// 開始抓資料
+// 2330 台積電
+// 2603 長榮
+// TODO: 需要從 stock.txt 的檔案裡讀取股票代碼
+let queryDate = moment().format('YYYYMMDD'); //'20220814';
+// axios.get(url, 設定)
+
+(async () => {
+  try {
+    let stockNo = await fs.promises.readFile('stockNo.txt', 'utf8');
+    // console.log(stockNo);
+    let response = await axios.get(`https://www.twse.com.tw/exchangeReport/STOCK_DAY`, {
+      params: {
+        response: 'json',
+        date: queryDate,
+        stockNo: stockNo,
+      },
+    });
+    let result = await axios.get('https://www.twse.com.tw/zh/api/codeQuery', {
+      params: {
+        query: stockNo,
+      },
+    });
+    let suggestion = result.data.suggestions[0];
+    let stockName = suggestion.split('\t').pop();
+    console.log(stockName);
+    console.log(response);
+  } catch (e) {
+    console.error(e);
+  }
+})();
