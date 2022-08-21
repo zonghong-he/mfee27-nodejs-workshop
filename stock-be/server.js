4902c762f88eebbe69af03ad6cd0905f0f166c0a
ZongHong <make75682@gmail.com>
2022-08-21 17:11:53 +0800

    api

--- stock-be/server.js
+++ stock-be/server.js
@@ -3,11 +3,51 @@ require('dotenv').config();
 const app = express();
 
 const port = process.env.SERVER_PORT;
+const mysql = require('mysql2');
+const cors = require('cors');
 
-app.get('/', (req, res) => {
-  res.send('Hello Express');
+const pool = mysql
+  .createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: 10,
+  })
+  .promise();
+
+app.use(cors());
+// app.get('/ssr', (req, res, next) => {
+//   res.render('index', {
+//     stocks: ['台積電', '長榮航', '聯發科'],
+//   });
+// });
+app.get('/api/stocks', async (req, res) => {
+  let [data] = await pool.execute('SELECT * FROM stocks');
+  res.json(data);
 });
 
+// app.use((req, res, next) => {
+//   console.log(1);
+//   next();
+// });
+// app.use((req, res, next) => {
+//   console.log(2);
+//   next();
+// });
+app.get('/', (req, res) => {
+  console.log('end');
+  res.send('Hello Express');
+});
+// app.get('/test', (req, res) => {
+//   console.log('end');
+//   res.send('Hello Express');
+// });
+app.use((req, res, next) => {
+  console.log('找不到網頁');
+  res.status(404).send('Oops! Not found!');
+});
 app.listen(port, () => {
   console.log(`server start at ${port}`);
 });
--- stock-fe/src/components/Stock.js
+++ stock-fe/src/components/Stock.js
@@ -1,20 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Stock = () => {
   const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    let getData = async () => {
+      let response = await axios.get('http://localhost:3001/api/stocks');
+      setData(response.data);
+    };
 
+    getData();
+  }, []);
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
   return (
     <div>
       {error && <div>{error}</div>}
       <h2 className="ml-7 mt-6 text-xl text-gray-600">股票代碼</h2>
 
-      <div className="bg-white bg-gray-50 p-6 rounded-lg shadow hover:shadow-lg m-6 cursor-pointer">
-        <Link to={`/stock/1234`}>
-          <h2 className="text-2xl font-bold mb-2 text-gray-800">1234</h2>
-          <p className="text-gray-700">1234名稱</p>
-        </Link>
-      </div>
+      {data.map((stock) => {
+        return (
+          <div className="bg-white bg-gray-50 p-6 rounded-lg shadow hover:shadow-lg m-6 cursor-pointer">
+            <Link to={`/stock/1234`}>
+              <h2 className="text-2xl font-bold mb-2 text-gray-800">{stock.id}</h2>
+              <p className="text-gray-700">{stock.name}</p>
+            </Link>
+          </div>
+        );
+      })}
     </div>
   );
 };
