6ba4b90a8d242042b0991e5902b53554af9952df
SINGsyung <make75682@gmail.com>
2022-09-04 10:43:13 +0800

    add session

--- stock-be/server.js
+++ stock-be/server.js
@@ -1,44 +1,139 @@
 const express = require('express');
-require('dotenv').config();
+// 利用 express 這個框架/函式庫 來建立一個 web application
 const app = express();
+// 初始化 dotenv
+require('dotenv').config();
 
-const port = process.env.SERVER_PORT;
-// const mysql = require('mysql2');
+const path = require('path');
+
+// 在程式碼中，不要讓某些常數散亂在專案的各處
+// 至少在同一個檔案中，可以放到最上方統一管理
+// 目標是: 只需要改一個地方，全部的地方就生效
+// 降低漏改到的風險 -> 降低程式出錯的風險
+const port = process.env.SERVER_PORT || 3002;
+
+// 啟用 session
+const expressSession = require('express-session');
+// 把 session 存在硬碟中
+var FileStore = require('session-file-store')(expressSession);
+app.use(
+  expressSession({
+    store: new FileStore({
+      // session 儲存的路徑
+      path: path.join(__dirname, '..', 'sessions'),
+    }),
+    secret: process.env.SESSION_SECRET,
+    // 如果 session 沒有改變的話，要不要重新儲存一次？
+    resave: false,
+    // 還沒初始化的，要不要存
+    saveUninitialized: false,
+  })
+);
+
+// npm i cors
 const cors = require('cors');
-
-const stocksRouter = require('./rooters/stocks');
-const authRouter = require('./rooters/auth');
+// 使用這個第三方提供的 cors 中間件
+// 來允許跨源存取
+// 預設都是全部開放
 app.use(cors());
-app.use(express.json());
-// app.get('/ssr', (req, res, next) => {
-//   res.render('index', {
-//     stocks: ['台積電', '長榮航', '聯發科'],
-//   });
-// });
-app.use(stocksRouter);
-app.use(authRouter);
-//設定網址變數
+// 使用情境: 當前後端網址不同時，只想允許自己的前端來跨源存取
+//          就可以利用 origin 這個設定來限制，不然預設是 * (全部)
+// const corsOptions = {
+//   origin: ['http://localhost:3000'],
+// };
+// app.use(cors(corsOptions));
 
-// app.use((req, res, next) => {
-//   console.log(1);
-//   next();
-// });
-// app.use((req, res, next) => {
-//   console.log(2);
-//   next();
-// });
-app.get('/', (req, res) => {
-  console.log('end');
+// 引用 server 需要的資料庫模組
+const pool = require('./utils/db');
+
+// 如果要讓 express 認得 json
+// Content-Type: application/json
+// 就要加上這個中間件
+app.use(express.json());
+
+// 設定視圖引擎，我們用的是 pug
+// npm i pug
+app.set('view engine', 'pug');
+// 告訴 express 視圖在哪裡
+app.set('views', 'views');
+
+// 設置靜態檔案
+// express.static => 讓靜態檔案可以有網址
+// http://localhost:3002/uploads/檔案名稱
+app.use(express.static(path.join(__dirname, 'public')));
+// 或是給 prefix
+// http://localhost:3002/public/uploads/檔案名稱
+// app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// 測試 server side render 的寫法
+app.get('/ssr', (req, res, next) => {
+  // views/index.pug
+  res.render('index', {
+    stocks: ['台積電', '長榮航', '聯發科'],
+  });
+});
+
+// express 是由 middleware 組成的
+// request -> middleware 1 -> middleware 2 -> ... -> reponse
+// 中間件的順序很重要!!
+// Express 會按照你程式碼的順序(由上到下)去決定 next 是誰
+// 中間件裡一定要有 next 或者 response
+// - next() 往下一關走
+// - res.xxx 結束這次的旅程 (req-res cycle)
+// pipeline pattern
+
+// 一般的 middleware
+app.use((req, res, next) => {
+  console.log('這是中間件 A');
+  let now = new Date();
+  console.log(`有人來訪問喔 at ${now.toISOString()}`);
+  // 一定要寫，讓 express 知道要跳去下一個中間件
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('這是中間件 C');
+  // 一定要寫，讓 express 知道要跳去下一個中間件
+  next();
+});
+
+// 路由中間件
+// app.[method]
+// method: get, post, delete, put, patch, ...
+// GET /
+app.get('/', (req, res, next) => {
+  console.log('這裡是首頁');
   res.send('Hello Express');
 });
-// app.get('/test', (req, res) => {
-//   console.log('end');
-//   res.send('Hello Express');
-// });
-app.use((req, res) => {
-  console.log('找不到網頁');
-  res.status(404).send('Oops! Not found!');
+app.get('/test', (req, res, next) => {
+  console.log('這裡是 test 1');
+  res.send('Hello Test 1');
+  // next();
 });
+
+let stockRouter = require('./rooters/stocks');
+app.use('/api/1.0/stocks', stockRouter);
+// /api/1.0/stocks
+// /api/1.0/stocks/:stockId
+
+let authRouter = require('./rooters/auth');
+app.use(authRouter);
+
+// app.get('/test', (req, res, next) => {
+//   console.log('這裡是 test 2');
+//   res.send('Hello Test 2');
+// });
+
+// 在所有的路由中間件的下面
+// 既然前面所有的「網址」都比不到，表示前面沒有任何符合的網址 (旅程一直沒有被結束)
+// --> 404
+// 利用這個特殊的順序，把這裡當成 404
+app.use((req, res, next) => {
+  console.log('在所有路由中間件的下面 -> 404 了！');
+  res.status(404).send('Not Found!!');
+});
+
+// 啟動 server，並且開始 listen 一個 port
 app.listen(port, () => {
   console.log(`server start at ${port}`);
 });
--- stock-fe/src/components/Login.js
+++ stock-fe/src/components/Login.js
@@ -1,4 +1,19 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { API_URL } from '../utils/config';
+
 const Login = () => {
+  const [loginMember, setLoginMember] = useState({ email: 'ashleylai58@gmail.com', password: 'testtest' });
+
+  function handleChange(e) {
+    setLoginMember({ ...loginMember, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    let response = await axios.post(`${API_URL}/auth/login`, loginMember);
+    console.log(response.data);
+  }
   return (
     <form className="bg-purple-100 h-screen md:h-full md:my-20 md:mx-16 lg:mx-28 xl:mx-40 py-16 md:py-8 px-24 text-gray-800 md:shadow md:rounded flex flex-col md:justify-center">
       <h2 className="flex justify-center text-3xl mb-6 border-b-2 pb-2 border-gray-300">登入帳戶</h2>
@@ -6,15 +21,31 @@ const Login = () => {
         <label htmlFor="name" className="flex mb-2 w-32">
           Email
         </label>
-        <input className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2" type="text" id="email" name="email" />
+        <input
+          className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2"
+          type="text"
+          id="email"
+          name="email"
+          value={loginMember.email}
+          onChange={handleChange}
+        />
       </div>
       <div className="mb-8 text-2xl">
         <label htmlFor="password" className="flex mb-2 w-16">
           密碼
         </label>
-        <input className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2" type="password" id="password" name="password" />
+        <input
+          className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2"
+          type="password"
+          id="password"
+          name="password"
+          value={loginMember.password}
+          onChange={handleChange}
+        />
       </div>
-      <button className="text-xl bg-indigo-300 px-4 py-2.5 rounded hover:bg-indigo-400 transition duration-200 ease-in">登入</button>
+      <button className="text-xl bg-indigo-300 px-4 py-2.5 rounded hover:bg-indigo-400 transition duration-200 ease-in" onClick={handleSubmit}>
+        登入
+      </button>
     </form>
   );
 };
